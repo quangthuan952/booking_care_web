7dc1dd5f8c24fbf7b43ec8ab49fe39dd9a7aeb3f
quangthuan952 <quangthuan9520@gmail.com>
2022-09-03 15:36:13 +0700

    update multi language

--- src/containers/HomePage/Header.js
+++ src/containers/HomePage/Header.js
@@ -3,12 +3,16 @@ import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import "./HomeHeader.scss"
 import { FormattedMessage } from 'react-intl';
-
+import {LANGUAGES} from "../../utils";
+import {changeLanguage} from "../../store/actions";
 class Header extends Component {
 
+
+    changeLanguage = (language) => {
+       this.props.changeLanguageAppRedux(language)
+    }
     render() {
-
-
+        const language = this.props.language
         return (
             <React.Fragment>
                 <div className="home-header-container">
@@ -40,15 +44,15 @@ class Header extends Component {
                                 <i className="fas fa-question-circle"/>
                                 <span><FormattedMessage id="homeHeader.support"/></span>
                             </div>
-                            <div className="language-vi"><FormattedMessage id="homeHeader.vn"/></div>
-                            <div className="language-en"><FormattedMessage id="homeHeader.en"/></div>
+                            <div className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}><FormattedMessage id="homeHeader.vn"/></span></div>
+                            <div className={language === LANGUAGES.EN ? "language-en active" : "language-en"}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}><FormattedMessage id="homeHeader.en"/></span></div>
                         </div>
                     </div>
                 </div>
                 <div className="home-header-banner">
                     <div className="content-up">
-                        <div className="title-1">nền tảng y tế</div>
-                        <div className="title-2">chăm sóc sức khỏe toàn diện</div>
+                        <div className="title-1"><FormattedMessage id="banner.medicalBackground"/></div>
+                        <div className="title-2"><FormattedMessage id="banner.comprehensiveHealthCare"/></div>
                         <div className="search">
                             <i className="fas fa-search"/>
                             <input type="text" placeholder="Tìm gói khám"/>
@@ -58,27 +62,27 @@ class Header extends Component {
                         <ul className="option-list">
                             <li className="option-item">
                                 <i className="fas fa-hospital"/>
-                                <div className="option-item-title">Khám Chuyên khoa</div>
+                                <div className="option-item-title"><FormattedMessage id="banner.specialistExamination"/></div>
                             </li>
                             <li className="option-item">
                                 <i className="fas fa-mobile-alt"/>
-                                <div className="option-item-title">Khám từ xa</div>
+                                <div className="option-item-title"><FormattedMessage id="banner.remoteExamination"/></div>
                             </li>
                             <li className="option-item">
                                 <i className="fas fa-procedures"/>
-                                <div className="option-item-title">Xét nghiệm y học</div>
+                                <div className="option-item-title"><FormattedMessage id="banner.medicalTest"/></div>
                             </li>
                             <li className="option-item">
                                 <i className="fas fa-stethoscope"/>
-                                <div className="option-item-title">Khám tổng quát</div>
+                                <div className="option-item-title"><FormattedMessage id="banner.generalExamination"/></div>
                             </li>
                             <li className="option-item">
                                 <i className="fas fa-user-md"/>
-                                <div className="option-item-title">Sức khỏe tinh thần</div>
+                                <div className="option-item-title"><FormattedMessage id="banner.mentalHealth"/></div>
                             </li>
                             <li className="option-item">
                                 <i className="fas fa-briefcase-medical"/>
-                                <div className="option-item-title">Sản phẩm y tế</div>
+                                <div className="option-item-title"><FormattedMessage id="banner.medicalProducts"/></div>
                             </li>
                         </ul>
                     </div>
@@ -97,7 +101,9 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-    return {};
+    return {
+        changeLanguageAppRedux: (language)  => dispatch(changeLanguage(language))
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
